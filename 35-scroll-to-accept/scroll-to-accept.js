4fd530824f826851c0a831f811fc679f25e85ad8
Stephen Joseph Muñez <sjtmunez@gmail.com>
2022-05-20 15:43:26 +0800

    finished scroll-to-accept

--- 35-scroll-to-accept/scroll-to-accept.js
+++ 35-scroll-to-accept/scroll-to-accept.js
@@ -1,1 +1,14 @@
-console.log("working");
+const terms = document.querySelector(".terms-and-conditions");
+const button = document.querySelector(".accept");
+
+function obCallback(payload) {
+  payload[0].intersectionRatio === 1
+    ? (button.disabled = false)
+    : (button.disabled = true);
+}
+const ob = new IntersectionObserver(obCallback, {
+  root: terms,
+  threshold: 0.1,
+});
+
+ob.observe(terms.lastElementChild);
